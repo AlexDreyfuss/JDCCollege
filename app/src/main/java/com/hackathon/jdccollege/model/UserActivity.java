9bc815c4697ff52a5c72c94c071656661819be97
AlexDreyfuss <arele94@gmail.com>
2017-01-05 08:58:34 +0200

    done and done again

--- app/src/main/java/com/hackathon/jdccollege/model/UserActivity.java
+++ app/src/main/java/com/hackathon/jdccollege/model/UserActivity.java
@@ -1,7 +1,12 @@
 package com.hackathon.jdccollege.model;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.hackathon.jdccollege.R;
 import com.hackathon.jdccollege.SqlDatabase.FactoryDB;
@@ -14,5 +19,30 @@ public class UserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
        // SqlDatabase db = FactoryDB.getDatabase();
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories_array, R.layout.spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.areas_array, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner2.setAdapter(adapter);
+
+        Button button= (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(UserActivity.this,Choose_course_activity.class);
+                startActivity(in);
+            }
+        });
     }
 }
